d01a9ec089893c85966e1fe6469eae21ccc54f06
Aimee <alramirez1@icloud.com>
2025-05-12 18:47:16 -0400

    theme

--- src/ThemeToggle.tsx
+++ src/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import React, {type FC} from 'react';
+import {type FC} from 'react';
 import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 type ThemeToggleProps = {
